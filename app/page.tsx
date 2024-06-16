66cd2e05c88815260f7a837953884aa6fd33391e
Aman Rawat <rawataman101@gmail.com>
2024-06-16 13:43:19 +0530

    Recoder fix + voice sync

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,30 @@ import Image from "next/image";
 import { SettingsIcon } from "lucide-react";
 import Messages from "@/components/Messages";
 import Recorder, { mimeType } from "@/components/Recorder";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useFormState } from "react-dom";
+import transcript from "@/actions/transcript";
+import VoiceSynthesizer from "@/components/VoiceSynthesizer";
+
+const initialState = {
+  sender: "",
+  response: "",
+  id: "",
+};
+
+export type Message = {
+  sender: string;
+  response: string;
+  id: string;
+};
+
 export default function Home() {
+  const [state, formAction] = useFormState(transcript, initialState);
   const fileRef = useRef<HTMLInputElement | null>(null);
   const submitButtonRef = useRef<HTMLButtonElement | null>(null);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [displaySettings, setDisplaySettings] = useState(false);
+
   //blob data object
   const uploadAudio = (blob: Blob) => {
     const url = URL.createObjectURL(blob);
@@ -25,19 +45,33 @@ export default function Home() {
     }
   };
 
-  const formAction = () => {};
+  // Responsible for updating the messages when the Server Action completes
+  useEffect(() => {
+    if (state.response && state.sender) {
+      setMessages((messages) => [
+        {
+          sender: state.sender || "",
+          response: state.response || "",
+          id: state.id || "",
+        },
+        ...messages,
+      ]);
+    }
+  }, [state]);
+
   return (
-    <main className="bg-black h-screen overflow-y-auto">
+    <main className="bg-black h-screen overflow-y-visible">
       {/* header */}
       <header className="flex justify-between fixed top-0 w-full text-white p-5">
         <Image
-          className="object-contain"
+          className="object-contain rounded-full"
           src="https://cdn.dribbble.com/users/398475/screenshots/19434406/media/378ea75ccc6fd1b40baa093fd63b95bf.gif?resize=400x0"
-          width={50}
-          height={50}
+          width={80}
+          height={40}
           alt="Picture of Siri"
         />
         <SettingsIcon
+          onClick={() => setDisplaySettings(!displaySettings)}
           size={40}
           className="p-2 m-2 rounded-full cursor-pointer bg-purple-600 text-black transition-all ease-in-out duration-150 hover:bg-purple-700 hover:text-white"
         />
@@ -51,10 +85,14 @@ export default function Home() {
         {/* hidden */}
         <input type="file" name="audio" hidden ref={fileRef} />
         <button type="submit" hidden ref={submitButtonRef}></button>
-        <div className="fixed bottom-0 w-full overflow-hidden bg-black rounded-3xl">
+        <div className="fixed bottom-0 w-full overflow-hidden bg-black rounded-t-3xl">
           {/* recorder */}
           <Recorder uploadAudio={uploadAudio} />
-          <div> {/* voice synthesier - output of the Assistant voice*/}</div>
+          <div>
+            {" "}
+            {/* voice synthesier - output of the Assistant voice*/}
+            <VoiceSynthesizer state={state} displaySettings={displaySettings} />
+          </div>
         </div>
       </form>
     </main>
